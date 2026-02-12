0fab7612dfc6edbe3f9c40e0da763b0c7da74051
chaosbenni0609-beep <chaosbenni0609@gmail.com>
2026-02-12 15:19:46 +0000

    asdadasdasfdxyx

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,12 @@
-import streamlit as st
-import pandas as pd
-import mysql.connector
-from mysql.connector import Error
 
-#Create connection
-def test_connection():
-    try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="user",  # Replace with your password
-            database="datab"  # Replace with your database name
-        )
-        if connection.is_connected():
-            print("Connection successful!")
-            return connection
-    except Error as e:
-        print(f"Error: {e}")
- 
-#test_connection()
-#fetch records
-def fetchData():
-    conn = test_connection()
-    if conn:
-        try:
-            query = "SELECT * FROM tab"
-            donors = pd.read_sql(query, conn)
-            return donors
-        except Error as e:
-            st.error(f"Error while fetching donor data: {str(e)}")
-        finally:
-            conn.close()
-    else:
-        return pd.DataFrame()  # Return an empty DataFrame if connection fails
-    
-st.title("Blood donation camp")
-st.subheader("Registered users")
-df=fetchData()
-st.dataframe(df)
-st.subheader("Registration - Form")
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from mytable;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
