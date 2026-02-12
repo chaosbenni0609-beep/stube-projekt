9374029e140f0cbf684205d2e6604dc15ef9885f
chaosbenni0609-beep <chaosbenni0609@gmail.com>
2026-02-12 15:45:36 +0000

    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 
 import streamlit as st
+import sqlalchemy 
+import mysqlclient
 
-st.write("asfasd")
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from mytable;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
