d6e13ef4bc0a7db7bec3d0728ccc9cc55c37eb93
chaosbenni0609-beep <chaosbenni0609@gmail.com>
2026-02-13 12:49:08 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 User_in = []
 User_in.append(st.slider("Frage 1 :",1,5,3))
-print(User_in)
+st.write(User_in)
 User_in.append(st.slider("Frage 2 :",1,5,3))
-print(User_in)
+st.write(User_in)
 User_in.append(st.slider("Frage 3 :",1,5,3))
-print(User_in)
+st.write(User_in)
